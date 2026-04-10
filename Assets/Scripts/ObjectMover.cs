9034048598612192b4b17acf1684cd5b85b7a2e1
Vinny Keeler <101831569+ShieldedAsh@users.noreply.github.com>
2026-04-10 13:05:49 -0400

    Initial movement for objects behind shoji screen

---
+++ Assets/Scripts/ObjectMover.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+/*ObjectMover.cs
+The purpose of this script is to move the objects behind the Shoji screen.
+*/
+public class ObjectMover : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rb; //rigidbody ref
+
+    [SerializeField]
+    //value at which the object will despawn
+    //Note: this implementation assumes objects can only move from left to right, could potentially be refactored later
+    //to allow for right to left movement.
+    private int despawnThreshold = 20;
+
+    [SerializeField]
+    //the speed at which the object moves
+    private float movementSpeed = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //destroy object if it is past despawn threshold
+        if (transform.position.x >= despawnThreshold)
+        {
+            Destroy(this.gameObject);
+        }
+
+        //Move the object at a fixed rate
+        rb.linearVelocityX = movementSpeed;
+    }
+}
