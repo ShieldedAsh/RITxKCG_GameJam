4479e82f2036e9daf6dee088d48fdcd99f7638c5
Chess <143632566+yonakoi@users.noreply.github.com>
2026-04-12 21:46:35 -0400

    girl animations!!

--- Assets/Scripts/Benchmarker.cs
+++ Assets/Scripts/Benchmarker.cs
@@ -12,29 +12,43 @@ public class Benchmarker : MonoBehaviour
     private GameObject cGrade;
     [SerializeField]
     private GameObject dGrade;
-    
+
+    [SerializeField]
+    private Animator shojiGirl;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         if (ObjectManager.totalScore >= 99999)
         {
             sGrade.SetActive(true);
+            PlayAnim("Surprised");
         }
         else if (ObjectManager.totalScore >= 79999)
         {
             aGrade.SetActive(true);
+            PlayAnim("Laughing");
         }
         else if (ObjectManager.totalScore >= 59999)
         {
             bGrade.SetActive(true);
+            PlayAnim("Wink");
         }
         else if (ObjectManager.totalScore >= 39999)
         {
             cGrade.SetActive(true);
+            PlayAnim("Think");
         }
         else if (ObjectManager.totalScore <= 20000)
         {
             dGrade.SetActive(true);
+            PlayAnim("Angry");
         }
     }
+
+    void PlayAnim(string anim)
+    {
+        shojiGirl.CrossFade(anim, 0.2f);
+    }
+
 }
