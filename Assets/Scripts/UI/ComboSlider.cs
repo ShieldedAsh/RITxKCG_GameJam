11b9f0709df7213f4b41dc5a0cb1d3a52fcb8e64
sakuraitowa <yingjingxianghe@gmail.com>
2026-04-16 14:23:00 +0900

    コンボカウントの追加

---
+++ Assets/Scripts/UI/ComboSlider.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class SimpleViewSlider : MonoBehaviour
+{
+    
+
+    private float width;
+
+    private void Start()
+    {
+    }
+
+    public void SetValue(float value)
+    {
+    }
+
+}
