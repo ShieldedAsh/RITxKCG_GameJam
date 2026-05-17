3d903da58789c6645b6f829b1b1f5b7378bf89ea
reiyu425 <reiyu425@icloud.com>
2026-05-18 03:12:37 +0900

    設定画面のイラスト追加

---
+++ Assets/Scripts/UI/CheckBoxButton.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckBoxButton : MonoBehaviour
+{
+    [SerializeField]
+    private Language language;
+
+    [SerializeField]
+    private GameObject myCheckImage;
+
+    [SerializeField]
+    private GameObject notMyCheckImage;
+
+    void Start()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(() =>
+        {
+            myCheckImage.SetActive(true);
+            notMyCheckImage.SetActive(false);
+            SettingsManager.Instance.SetLanguage(language);
+        });
+    }
+}
